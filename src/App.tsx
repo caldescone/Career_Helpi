c715c9f219530499f96f85c13355fcc5dcde0e25
ejciarlo <89992730+ejciarlo@users.noreply.github.com>
2024-04-04 09:25:40 -0400

    Added Name

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,7 @@ function App() {
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
       </Form>
+      <div>Ethan Ciarlo</div>
     </div>
   );
 }
