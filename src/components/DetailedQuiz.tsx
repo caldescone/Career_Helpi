be5a6a41382949f3c949f377e20365ec647381b5
Connor Westerman <cjwest@udel.edu>
2024-04-10 22:04:50 -0400

    Added question spaces to the detailed quiz to be filled in once we agree on the questions

--- src/components/DetailedQuiz.tsx
+++ src/components/DetailedQuiz.tsx
@@ -6,12 +6,34 @@ export default function DetailedQuiz() {
   const totalQuestions = 15;
 
   return (
-    <div className="quiz">
-      <ProgressBar
-        questionsComplete={questionsComplete}
-        totalQuestions={totalQuestions}
-      />
-      <h1> Detailed Quiz</h1>
+    <div>
+    <ProgressBar
+      questionsComplete={questionsComplete}
+      totalQuestions={totalQuestions}
+    />
+    <h1> Detailed Quiz</h1>
+
+    <hr></hr>
+
+    <div className="row">
+      <div className="column">
+        <ol>
+          <li>Question 1</li>
+          <li>Question 2</li>
+          <li>Question 3</li>
+          <li>Question 4</li>
+        </ol>
+      </div>
+      <div className="column">
+        <ol>
+          <li>Question 5</li>
+          <li>Question 6</li>
+          <li>Question 7</li>
+          <li>Question 8</li>
+        </ol>
+      </div>
     </div>
+  <hr></hr>
+</div>
   );
 }
