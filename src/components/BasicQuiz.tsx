17386ac08d3d5c4509a6192d8bc0dea6a660ca8e
Connor Westerman <cjwest@udel.edu>
2024-04-11 00:40:37 -0400

    Added the descriptions of the quizzes to the home page. Will discuss later how we want the page to be formatted.

--- src/components/BasicQuiz.tsx
+++ src/components/BasicQuiz.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 import ProgressBar from "./ProgressBar";
 
 export default function BasicQuiz() {
@@ -11,25 +12,39 @@ export default function BasicQuiz() {
         questionsComplete={questionsComplete}
         totalQuestions={totalQuestions}
       />
-      <h1> Basic Quiz</h1>
-
+      <h1> <u> Basic Quiz </u> </h1>
       <hr></hr>
 
       <div className="row">
         <div className="column">
           <ol>
             <li>Question 1</li>
+            
+            <br></br>
             <li>Question 2</li>
+            
+            <br></br>
             <li>Question 3</li>
+            
+            <br></br>
             <li>Question 4</li>
+            
           </ol>
         </div>
         <div className="column">
           <ol>
             <li>Question 5</li>
+            
+            <br></br>
             <li>Question 6</li>
+            
+            <br></br>
             <li>Question 7</li>
+            
+            <br></br>
             <li>Question 8</li>
+            
+            <br></br>
           </ol>
         </div>
       </div>
--- src/components/DetailedQuiz.tsx
+++ src/components/DetailedQuiz.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 import ProgressBar from "./ProgressBar";
 
 export default function DetailedQuiz() {
@@ -11,28 +12,43 @@ export default function DetailedQuiz() {
       questionsComplete={questionsComplete}
       totalQuestions={totalQuestions}
     />
-    <h1> Detailed Quiz</h1>
+    <h1> <u> Detailed Quiz </u> </h1>
 
     <hr></hr>
 
     <div className="row">
-      <div className="column">
-        <ol>
-          <li>Question 1</li>
-          <li>Question 2</li>
-          <li>Question 3</li>
-          <li>Question 4</li>
-        </ol>
+        <div className="column">
+          <ol>
+            <li>Question 1</li>
+            
+            <br></br>
+            <li>Question 2</li>
+            
+            <br></br>
+            <li>Question 3</li>
+            
+            <br></br>
+            <li>Question 4</li>
+            
+          </ol>
+        </div>
+        <div className="column">
+          <ol>
+            <li>Question 5</li>
+            
+            <br></br>
+            <li>Question 6</li>
+            
+            <br></br>
+            <li>Question 7</li>
+            
+            <br></br>
+            <li>Question 8</li>
+            
+            <br></br>
+          </ol>
+        </div>
       </div>
-      <div className="column">
-        <ol>
-          <li>Question 5</li>
-          <li>Question 6</li>
-          <li>Question 7</li>
-          <li>Question 8</li>
-        </ol>
-      </div>
-    </div>
   <hr></hr>
 </div>
   );
