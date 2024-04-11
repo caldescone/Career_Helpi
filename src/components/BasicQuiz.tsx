f3ce9dc1b93681a5d1657632b6d819f737b4dbbe
Connor Westerman <cjwest@udel.edu>
2024-04-11 01:16:32 -0400

    Added a yes/no answer box for the basic quiz and a fill in the blank answer box for the detailed quiz

--- src/components/BasicQuiz.tsx
+++ src/components/BasicQuiz.tsx
@@ -6,6 +6,45 @@ export default function BasicQuiz() {
   const [questionsComplete] = useState<number>(12);
   const totalQuestions = 15;
 
+  
+  let options: string[] = ["","Yes","No"]
+
+  const defaultOption = options[0];
+  const [selected1, setSelected1] = useState<string>(defaultOption);
+  const [selected2, setSelected2] = useState<string>(defaultOption);
+  const [selected3, setSelected3] = useState<string>(defaultOption);
+  const [selected4, setSelected4] = useState<string>(defaultOption);
+  const [selected5, setSelected5] = useState<string>(defaultOption);
+  const [selected6, setSelected6] = useState<string>(defaultOption);
+  const [selected7, setSelected7] = useState<string>(defaultOption);
+  const [selected8, setSelected8] = useState<string>(defaultOption);
+
+  function updateSelected1(event: React.ChangeEvent<HTMLSelectElement>) {
+      setSelected1(event.target.value);
+  }
+  function updateSelected2(event: React.ChangeEvent<HTMLSelectElement>) {
+    setSelected2(event.target.value);
+  }
+  function updateSelected3(event: React.ChangeEvent<HTMLSelectElement>) {
+    setSelected3(event.target.value);
+  }
+  function updateSelected4(event: React.ChangeEvent<HTMLSelectElement>) {
+    setSelected4(event.target.value);
+  }
+  function updateSelected5(event: React.ChangeEvent<HTMLSelectElement>) {
+    setSelected5(event.target.value);
+  }
+  function updateSelected6(event: React.ChangeEvent<HTMLSelectElement>) {
+    setSelected6(event.target.value);
+  }
+  function updateSelected7(event: React.ChangeEvent<HTMLSelectElement>) {
+    setSelected7(event.target.value);
+  }
+  function updateSelected8(event: React.ChangeEvent<HTMLSelectElement>) {
+    setSelected8(event.target.value);
+  }
+
+
   return (
     <div>
       <ProgressBar
@@ -19,31 +58,134 @@ export default function BasicQuiz() {
         <div className="column">
           <ol>
             <li>Question 1</li>
-            
+            <div>
+                <Form.Group>
+                    {/* <Form.Label>How do you feel?</Form.Label> */}
+                    <Form.Select value={selected1} onChange={updateSelected1}>
+                        {options.map((choice: string) => (
+                            <option key={choice} value={choice}>
+                                {choice}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                {selected1 === "Yes" || selected1 === "No" ? "✔️" : "❌"}
+            </div>
             <br></br>
+
             <li>Question 2</li>
-            
+            <div>
+                <Form.Group>
+                    {/* <Form.Label>How do you feel?</Form.Label> */}
+                    <Form.Select value={selected2} onChange={updateSelected2}>
+                        {options.map((choice: string) => (
+                            <option key={choice} value={choice}>
+                                {choice}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                {selected2 === "Yes" || selected2 === "No" ? "✔️" : "❌"}
+            </div>
             <br></br>
+
             <li>Question 3</li>
-            
+            <div>
+                <Form.Group>
+                    {/* <Form.Label>How do you feel?</Form.Label> */}
+                    <Form.Select value={selected3} onChange={updateSelected3}>
+                        {options.map((choice: string) => (
+                            <option key={choice} value={choice}>
+                                {choice}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                {selected3 === "Yes" || selected3 === "No" ? "✔️" : "❌"}
+            </div>
             <br></br>
+
             <li>Question 4</li>
-            
+            <div>
+                <Form.Group>
+                    {/* <Form.Label>How do you feel?</Form.Label> */}
+                    <Form.Select value={selected4} onChange={updateSelected4}>
+                        {options.map((choice: string) => (
+                            <option key={choice} value={choice}>
+                                {choice}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                {selected4 === "Yes" || selected4 === "No" ? "✔️" : "❌"}
+            </div>
           </ol>
         </div>
+
         <div className="column">
           <ol>
             <li>Question 5</li>
-            
+            <div>
+                <Form.Group>
+                    {/* <Form.Label>How do you feel?</Form.Label> */}
+                    <Form.Select value={selected5} onChange={updateSelected5}>
+                        {options.map((choice: string) => (
+                            <option key={choice} value={choice}>
+                                {choice}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                {selected5 === "Yes" || selected5 === "No" ? "✔️" : "❌"}
+            </div>
             <br></br>
+
             <li>Question 6</li>
-            
+            <div>
+                <Form.Group>
+                    {/* <Form.Label>How do you feel?</Form.Label> */}
+                    <Form.Select value={selected6} onChange={updateSelected6}>
+                        {options.map((choice: string) => (
+                            <option key={choice} value={choice}>
+                                {choice}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                {selected6 === "Yes" || selected6 === "No" ? "✔️" : "❌"}
+            </div>
             <br></br>
+
             <li>Question 7</li>
-            
+            <div>
+                <Form.Group>
+                    {/* <Form.Label>How do you feel?</Form.Label> */}
+                    <Form.Select value={selected7} onChange={updateSelected7}>
+                        {options.map((choice: string) => (
+                            <option key={choice} value={choice}>
+                                {choice}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                {selected7 === "Yes" || selected7 === "No" ? "✔️" : "❌"}
+            </div>
             <br></br>
-            <li>Question 8</li>
             
+            <li>Question 8</li>
+            <div>
+                <Form.Group>
+                    {/* <Form.Label>How do you feel?</Form.Label> */}
+                    <Form.Select value={selected8} onChange={updateSelected8}>
+                        {options.map((choice: string) => (
+                            <option key={choice} value={choice}>
+                                {choice}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                {selected8 === "Yes" || selected8 === "No" ? "✔️" : "❌"}
+            </div>
             <br></br>
           </ol>
         </div>
--- src/components/DetailedQuiz.tsx
+++ src/components/DetailedQuiz.tsx
@@ -6,6 +6,42 @@ export default function DetailedQuiz() {
   const [questionsComplete] = useState<number>(12);
   const totalQuestions = 15;
 
+
+  const [answer1, setAnswer1] = useState<string>("");
+  const [answer2, setAnswer2] = useState<string>("");
+  const [answer3, setAnswer3] = useState<string>("");
+  const [answer4, setAnswer4] = useState<string>("");
+  const [answer5, setAnswer5] = useState<string>("");
+  const [answer6, setAnswer6] = useState<string>("");
+  const [answer7, setAnswer7] = useState<string>("");
+  const [answer8, setAnswer8] = useState<string>("");
+
+  function updateAnswer1(event: React.ChangeEvent<HTMLInputElement>) {
+    setAnswer1(event.target.value);
+  }
+  function updateAnswer2(event: React.ChangeEvent<HTMLInputElement>) {
+    setAnswer2(event.target.value);
+  }
+  function updateAnswer3(event: React.ChangeEvent<HTMLInputElement>) {
+    setAnswer3(event.target.value);
+  }
+  function updateAnswer4(event: React.ChangeEvent<HTMLInputElement>) {
+    setAnswer4(event.target.value);
+  }
+  function updateAnswer5(event: React.ChangeEvent<HTMLInputElement>) {
+    setAnswer5(event.target.value);
+  }
+  function updateAnswer6(event: React.ChangeEvent<HTMLInputElement>) {
+    setAnswer6(event.target.value);
+  }
+  function updateAnswer7(event: React.ChangeEvent<HTMLInputElement>) {
+    setAnswer7(event.target.value);
+  }
+  function updateAnswer8(event: React.ChangeEvent<HTMLInputElement>) {
+    setAnswer8(event.target.value);
+  }
+
+
   return (
     <div>
     <ProgressBar
@@ -20,31 +56,71 @@ export default function DetailedQuiz() {
         <div className="column">
           <ol>
             <li>Question 1</li>
-            
+            <div>
+              <Form.Group controlId="formCheckAnswer">
+                  <Form.Label>Answer:</Form.Label>
+                  <Form.Control value={answer1} onChange={updateAnswer1} />
+              </Form.Group> {answer1.length > 3 ? "✔️" : "❌"}
+            </div>
             <br></br>
             <li>Question 2</li>
-            
+            <div>
+              <Form.Group controlId="formCheckAnswer">
+                  <Form.Label>Answer:</Form.Label>
+                  <Form.Control value={answer2} onChange={updateAnswer2} />
+              </Form.Group> {answer2.length > 3 ? "✔️" : "❌"}
+            </div>
             <br></br>
             <li>Question 3</li>
-            
+            <div>
+              <Form.Group controlId="formCheckAnswer">
+                  <Form.Label>Answer:</Form.Label>
+                  <Form.Control value={answer3} onChange={updateAnswer3} />
+              </Form.Group> {answer3.length > 3 ? "✔️" : "❌"}
+            </div>
             <br></br>
             <li>Question 4</li>
-            
+            <div>
+              <Form.Group controlId="formCheckAnswer">
+                  <Form.Label>Answer:</Form.Label>
+                  <Form.Control value={answer4} onChange={updateAnswer4} />
+              </Form.Group> {answer4.length > 3 ? "✔️" : "❌"}
+            </div>
           </ol>
         </div>
         <div className="column">
           <ol>
             <li>Question 5</li>
-            
+            <div>
+              <Form.Group controlId="formCheckAnswer">
+                  <Form.Label>Answer:</Form.Label>
+                  <Form.Control value={answer5} onChange={updateAnswer5} />
+              </Form.Group> {answer5.length > 3 ? "✔️" : "❌"}
+            </div>
             <br></br>
             <li>Question 6</li>
-            
+            <div>
+              <Form.Group controlId="formCheckAnswer">
+                  <Form.Label>Answer:</Form.Label>
+                  <Form.Control value={answer6} onChange={updateAnswer6} />
+              </Form.Group> {answer6.length > 3 ? "✔️" : "❌"}
+            </div>
             <br></br>
             <li>Question 7</li>
-            
+            <div>
+              <Form.Group controlId="formCheckAnswer">
+                  <Form.Label>Answer:</Form.Label>
+                  <Form.Control value={answer7} onChange={updateAnswer7} />
+              </Form.Group> {answer7.length > 3 ? "✔️" : "❌"}
+            </div>
             <br></br>
             <li>Question 8</li>
-            
+            <div>
+              <Form.Group controlId="formCheckAnswer">
+                  <Form.Label>Answer:</Form.Label>
+                  <Form.Control value={answer8} onChange={updateAnswer8} />
+              </Form.Group> {answer8.length > 3 ? "✔️" : "❌"}
+            </div>
             <br></br>
           </ol>
         </div>
