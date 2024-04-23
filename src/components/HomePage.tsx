ae96bc2cfccff5d71f219e5a15150af4cfe5c6c6
Connor Westerman <cjwest@udel.edu>
2024-04-23 11:57:04 -0400

    Added to the description

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,17 +3,23 @@ export default function HomePage() {
     <div className="row">
             <div className="columnCenter">
             <h1> <u>Basic Quiz Description</u> </h1>
-            A quicker quiz filled with more basic questions. 
+            <b>
+            A quicker quiz filled with more basic questions. This quiz lets you choose between a few 
+            selectable options in order to quickly get your results.
+            </b>
             <br></br>
             <br></br>
-            Estimated time: 5 Minutes
+            <b>Estimated time: 5 Minutes</b>
             </div>
             <div className="columnCenter">
             <h1> <u>Detailed Quiz Description</u> </h1> 
-            A longer quiz filled with harder, more detailed questions.
+            <b>
+            A longer quiz filled with harder, more detailed questions. This quiz has fill-in text boxes 
+            to give more in-depth answers in order to get more accurate results.
+            </b>
             <br></br>
             <br></br>
-            Estimated time: 10 Minutes
+            <b>Estimated time: 10 Minutes</b>
             </div>
         </div>
     )
