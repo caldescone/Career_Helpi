54d060f162e30ae68e55692ae7204ed9f6b6802f
Connor Westerman <cjwest@udel.edu>
2024-05-02 13:47:20 -0400

    Deleted nav buttons at bottom of page

--- src/App.tsx
+++ src/App.tsx
@@ -55,25 +55,6 @@ function App() {
         {currentPage === "basic" ? <BasicQuiz keyData={keyData} /> : null}
         {currentPage === "detailed" ? <DetailedQuiz keyData={keyData} /> : null}
 
-        <Button
-          className="Home-Button mt-5 mx-auto mb-3"
-          onClick={() => setCurrentPage("home")}
-        >
-          Home Page
-        </Button>
-        <Button
-          className="Basic-Button mt-5 mx-auto mb-3"
-          onClick={() => setCurrentPage("basic")}
-        >
-          Basic Quiz
-        </Button>
-        <Button
-          className="Detailed-Button mt-5 mx-auto mb-3"
-          onClick={() => setCurrentPage("detailed")}
-        >
-          Detailed Quiz
-        </Button>
-
         <CardFooter>
           <Form>
             <Form.Label className="d-block text-center mt-2">
