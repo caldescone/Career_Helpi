c49917b244e3b8efe5d5b5b00136bafe6c0d3ebf
Christopher Calderone <chriscalderone19@gmail.com>
2024-05-09 17:35:05 -0400

    Fixed a saving issue in my navbar and Basic/detailed quiz files

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,8 @@
 // import HomePage from "./HomePage";
 // import BasicQuiz from "./BasicQuiz";
 // import DetailedQuiz from "./DetailedQuiz";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 function NavBar({
@@ -18,8 +20,23 @@ function NavBar({
     | "basicSpanish"
     | "detailedSpanish"
     | "homeSpanish";
-  setCurrentPage: (page: "detailed" | "basic" | "home") => void;
+  setCurrentPage: (
+    page:
+      | "detailed"
+      | "basic"
+      | "home"
+      | "basicSpanish"
+      | "detailedSpanish"
+      | "homeSpanish"
+  ) => void;
 }) {
+  const [isSpanish, setIsSpanish] = useState<boolean>(true);
+
+  // This is the Control
+  function updateLanguage(event: React.ChangeEvent<HTMLInputElement>) {
+    setCurrentPage(isSpanish ? "homeSpanish" : "home");
+    setIsSpanish(!isSpanish);
+  }
   return (
     <nav className="navbar fixed-top navbar-dark bg-dark shadow">
       <div className="container-fluid">
@@ -33,6 +50,16 @@ function NavBar({
           />
           <span className="fw-bolder fs-4">{brandName}</span>
         </div>
+        {currentPage === "home" || currentPage === "homeSpanish" ? (
+          <Form.Check
+            type="switch"
+            id="is-spanish-check"
+            label={isSpanish ? "English" : "Español"}
+            checked={isSpanish}
+            onChange={updateLanguage}
+            className="white-label" // Add a CSS class for styling
+          />
+        ) : null}
         <NavDropdown title="Menu" id="basic-nav-dropdown">
           <NavDropdown.Item onClick={() => setCurrentPage("home")}>
             Home
@@ -40,8 +67,14 @@ function NavBar({
           <NavDropdown.Item onClick={() => setCurrentPage("basic")}>
             Basic Quiz
           </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => setCurrentPage("basicSpanish")}>
+            Quiz Básico
+          </NavDropdown.Item>
           <NavDropdown.Item onClick={() => setCurrentPage("detailed")}>
             Detailed Quiz
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => setCurrentPage("detailedSpanish")}>
+            Quiz Detallado
           </NavDropdown.Item>
         </NavDropdown>
       </div>
