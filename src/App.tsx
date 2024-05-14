8827df5ff3979990df434da2ca241a34edc595ff
John Fulkerson <johnfulky@mac.com>
2024-05-14 12:11:20 -0400

    finished api key

--- src/App.tsx
+++ src/App.tsx
@@ -67,19 +67,19 @@ function App() {
         <CardFooter>
           <Form>
             <Form.Label className="d-block text-center mt-2">
-              API Key / Clave de API:
+              {isSpanish ? "Clave API" : "API Key"}
               <Form.Control
                 id={`api`} // unique id for each input from chrome suggestions
                 name={`api`} // unique name for each input from chrome suggestions
                 type="password"
-                placeholder="Insert API Key Here / Inserte la Clave de API Aquí"
+                placeholder={isSpanish ? "Inserte la clave API aquí" : "Insert API Key Here"}
                 onChange={changeKey}
                 style={{ width: "800px", margin: "0 auto" }}
               ></Form.Control>
             </Form.Label>
 
             <Button className="Submit-Button" onClick={handleSubmit}>
-              Submit / Enviar
+              {isSpanish ? "Enviar": "Submit"}
             </Button>
           </Form>
         </CardFooter>
